42c2860a8a4b27e394a51d3ddc30a937c0f588a0
lkamrat <kamrat9@gmail.com>
2017-03-02 08:01:34 +0200

    Add project files.

---
+++ HU_L7_Class/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HU_L7_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
